bdf1d76082373ce37968ae3cadc1bd16454a6f81
ztcaoll222 <admin@ztcaoll222.cn>
2017-02-11 14:01:04 +0800

    增加上传

--- ftp.py
+++ ftp.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, send_from_directory, flash
+from flask import Flask, render_template, send_from_directory, flash, request, redirect, url_for, session
 
 SECRET_KEY = 'g6a1g91g91SASDd1f+a1f1'
 CURRENT_DIR = os.getcwd()
@@ -11,9 +11,42 @@ app.config.from_object(__name__)
 def dir_list(path):
     return [x for x in os.listdir(path)]
 
-@app.route('/', methods=['GET'])
+# copy from werkzeug.utils import secure_filename
+# to support the chinese
+def my_secure_filename(filename):
+    import sys
+    import re
+
+    text_type = str
+    PY2 = sys.version_info[0] == 2
+    _filename_ascii_strip_re = re.compile(r'[^A-Za-z0-9\u4e00-\u9fa5_.-]')
+    _windows_device_files = ('CON', 'AUX', 'COM1', 'COM2', 'COM3', 'COM4', 'LPT1',
+                             'LPT2', 'LPT3', 'PRN', 'NUL')
+
+    if isinstance(filename, text_type):
+        from unicodedata import normalize
+        filename = normalize('NFKD', filename).encode('utf-8', 'ignore')
+        if not PY2:
+            filename = filename.decode('utf-8')
+    for sep in os.path.sep, os.path.altsep:
+        if sep:
+            filename = filename.replace(sep, ' ')
+    filename = str(_filename_ascii_strip_re.sub('', '_'.join(
+                   filename.split()))).strip('._')
+
+    if os.name == 'nt' and filename and \
+       filename.split('.')[0].upper() in _windows_device_files:
+        filename = '_' + filename
+
+    return filename
+
+@app.route('/')
 @app.route('/<path:Path>', methods=['GET'])
-def ftp_get(Path = ''):
+def ftp(Path = ''):
+    if session.get('Path'):
+        session.pop('Path')
+        session.pop('path')
+
     path = os.path.join(CURRENT_DIR, Path)
 
     if os.path.isfile(path):
@@ -60,12 +93,48 @@ def ftp_get(Path = ''):
     except:
         up = ''
 
-    return render_template('base.html',
-                           title = Path,
-                           up = up,
-                           dirList = dir_list(path),
-                           Path = Path)
+    session['Path'] = Path
+    session['path'] = path
 
+    return render_template('base.html',
+                           title=Path,
+                           upload=False,
+                           up=up,
+                           Path=Path,
+                           dirList=dir_list(path))
+
+@app.route('/upload', methods=['GET'])
+def upload_get():
+    Path = session['Path']
+    path = session['path']
+
+    Path = Path + '/'
+
+    try:
+        up = Path.split('/')
+        up.pop(-1)
+        up.pop(-1)
+        up = '/'.join(up)
+    except:
+        up = ''
+
+    return render_template('base.html',
+                           title=Path,
+                           upload=True,
+                           up=up,
+                           Path=Path,
+                           dirList=dir_list(path))
+
+@app.route('/upload', methods=['POST'])
+def upload_post():
+    path = session['path']
+
+    file = request.files['file']
+    if file:
+        filename = my_secure_filename(file.filename)
+        file.save(os.path.join(path, filename))
+        return redirect(url_for('upload_get',
+                                filename=filename))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
