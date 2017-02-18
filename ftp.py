fdd464cce43ff3c3a3327531e3f7bb6fe1a590b8
ztcaoll222 <admin@ztcaoll222.cn>
2017-02-18 16:16:08 +0800

    完善
    上传进度条，增加复制，移动，删除功能

--- ftp.py
+++ ftp.py
@@ -1,22 +1,14 @@
 import os
-from flask import Flask, render_template, send_from_directory, flash, request, redirect, url_for, session
+import sys
+import re
+from flask import Flask, render_template, send_from_directory, flash, request, redirect, session, abort
+from shutil import rmtree, copyfile, copytree
+from unicodedata import normalize
 
-SECRET_KEY = 'g6a1g91g91SASDd1f+a1f1'
-CURRENT_DIR = os.getcwd()
-
-app = Flask(__name__)
-
-app.config.from_object(__name__)
-
-def dir_list(path):
-    return [x for x in os.listdir(path)]
 
 # copy from werkzeug.utils import secure_filename
 # to support the chinese
 def my_secure_filename(filename):
-    import sys
-    import re
-
     text_type = str
     PY2 = sys.version_info[0] == 2
     _filename_ascii_strip_re = re.compile(r'[^A-Za-z0-9\u4e00-\u9fa5_.-]')
@@ -24,7 +16,6 @@ def my_secure_filename(filename):
                              'LPT2', 'LPT3', 'PRN', 'NUL')
 
     if isinstance(filename, text_type):
-        from unicodedata import normalize
         filename = normalize('NFKD', filename).encode('utf-8', 'ignore')
         if not PY2:
             filename = filename.decode('utf-8')
@@ -32,22 +23,73 @@ def my_secure_filename(filename):
         if sep:
             filename = filename.replace(sep, ' ')
     filename = str(_filename_ascii_strip_re.sub('', '_'.join(
-                   filename.split()))).strip('._')
+        filename.split()))).strip('._')
 
     if os.name == 'nt' and filename and \
-       filename.split('.')[0].upper() in _windows_device_files:
+                    filename.split('.')[0].upper() in _windows_device_files:
         filename = '_' + filename
 
     return filename
 
-@app.route('/')
-@app.route('/<path:Path>', methods=['GET'])
-def ftp(Path = ''):
-    if session.get('Path'):
-        session.pop('Path')
-        session.pop('path')
 
-    path = os.path.join(CURRENT_DIR, Path)
+def dir_list(path):
+    return [x for x in os.listdir(path)]
+
+
+def file_pop():
+    session.pop('filePrePath', False)
+    session.pop('fileList', False)
+
+
+SECRET_KEY = 'SECRET_KEY'
+CURRENT_DIR = os.getcwd()
+
+app = Flask(__name__)
+
+app.config.from_object(__name__)
+
+
+@app.route('/favicon.ico', methods=['GET'])
+def favicon_ico():
+    abort(404)
+
+
+@app.route('/', methods=['GET'])
+def home_of_no_variable_get():
+    return redirect('/ftp')
+
+
+@app.route('/ftp/', methods=['GET'])
+def ftp_of_no_variable_get():
+    currentPath = ''
+    session['currentPath'] = currentPath
+    path = os.path.join(CURRENT_DIR, currentPath)
+
+    fileList = dir_list(path)
+
+    isUpload = session.get('isUpload', False)
+    session.pop('isUpload', False)
+
+    isCopy = session.get('isCopy', False)
+    # session.pop('isCopy', False)
+
+    isMov = session.get('isMov', False)
+    # session.pop('isMov', False)
+
+    return render_template('base.html',
+                           title = currentPath,
+                           upload = isUpload,
+                           path = currentPath,
+                           fileList = fileList,
+                           isCopy = isCopy,
+                           isMov = isMov)
+
+
+@app.route('/ftp/<path:path>', methods=['GET'])
+def ftp_of_variable_get(path):
+    currentPath = path
+    session['currentPath'] = currentPath
+    path = os.path.join(CURRENT_DIR, currentPath)
 
     if os.path.isfile(path):
         format = os.path.splitext(path)[1]
@@ -76,65 +118,155 @@ def ftp(Path = ''):
             res.headers['Content-Type'] = 'text/plain'
             return res
 
+        if format in ['.pdf']:
+            with open(path, 'rb') as f:
+                res = app.make_response(f.read())
+            res.headers['Content-Type'] = 'application/pdf'
+            return res
+
         return send_from_directory(os.path.split(path)[0],
                                    os.path.split(path)[1],
                                    as_attachment=True)
 
-    temp = '/'
-    if Path == '':
-        temp = ''
-    Path = temp + Path + '/'
+    fileList = dir_list(path)
 
-    try:
-        up = Path.split('/')
-        up.pop(-1)
-        up.pop(-1)
-        up = '/'.join(up)
-    except:
-        up = ''
+    up = currentPath.split('/')
+    up.pop(-1)
+    up = '/'.join(up)
 
-    session['Path'] = Path
-    session['path'] = path
+    isUpload = session.get('isUpload', False)
+    session.pop('isUpload', False)
+
+    isCopy = session.get('isCopy', False)
+    # session.pop('isCopy', False)
+
+    isMov = session.get('isMov', False)
+    # session.pop('isMov', False)
 
     return render_template('base.html',
-                           title=Path,
-                           upload=False,
-                           up=up,
-                           Path=Path,
-                           dirList=dir_list(path))
+                           title=currentPath,
+                           up = up,
+                           upload=isUpload,
+                           path = currentPath+'/',
+                           fileList = fileList,
+                           isCopy = isCopy,
+                           isMov = isMov)
+
 
 @app.route('/upload', methods=['GET'])
 def upload_get():
-    Path = session['Path']
-    path = session['path']
+    session['isUpload'] = True
+    return redirect('/ftp/%s' % session['currentPath'])
 
-    Path = Path + '/'
-
-    try:
-        up = Path.split('/')
-        up.pop(-1)
-        up.pop(-1)
-        up = '/'.join(up)
-    except:
-        up = ''
-
-    return render_template('base.html',
-                           title=Path,
-                           upload=True,
-                           up=up,
-                           Path=Path,
-                           dirList=dir_list(path))
 
 @app.route('/upload', methods=['POST'])
 def upload_post():
-    path = session['path']
+    currentPath = session['currentPath']
+    session.pop('isUpload', False)
 
     file = request.files['file']
+    filename = file.filename
     if file:
-        filename = my_secure_filename(file.filename)
-        file.save(os.path.join(path, filename))
-        return redirect(url_for('upload_get',
-                                filename=filename))
+        filename = my_secure_filename(filename)
+        file.save(os.path.join(currentPath, filename))
+        flash('%s upload success!' % filename)
+    else:
+        flash('%s upload unsuccess!' % filename)
+
+    return redirect('/ftp/%s' % currentPath)
+
+
+@app.route('/del', methods=['POST'])
+def del_post():
+    currentPath = session['currentPath']
+
+    choice = dict(request.form)
+    choice.pop('del')
+    choice = list(choice)
+
+    for filename in choice:
+        path = os.path.join(currentPath, filename)
+        if os.path.isfile(path):
+            os.remove(path)
+        else:
+            rmtree(path)
+
+    flash('del %s success!' % choice)
+    return redirect('/ftp/%s' % currentPath)
+
+
+@app.route('/copy', methods=['POST'])
+def copy_post():
+    currentPath = session['currentPath']
+
+    choice = dict(request.form)
+    choice.pop('copy')
+    choice = list(choice)
+
+    session['filePrePath'] = session['currentPath']
+    session['fileList'] = choice
+    session['isCopy'] = True
+
+    session.pop('isMov', False)
+
+    return redirect('/ftp/%s' % currentPath)
+
+
+@app.route('/mov', methods=['POST'])
+def mov_post():
+    currentPath = session['currentPath']
+
+    choice = dict(request.form)
+    choice.pop('mov')
+    choice = list(choice)
+
+    session['filePrePath'] = session['currentPath']
+    session['fileList'] = choice
+    session['isMov'] = True
+
+    session.pop('isCopy', False)
+
+    return redirect('/ftp/%s' % currentPath)
+
+
+@app.route('/paste', methods=['POST'])
+def paste_post():
+    currentPath = session['currentPath']
+
+    fileList = session['fileList']
+    for filename in fileList:
+        src = os.path.join(session['filePrePath'], filename)
+        dst = os.path.join(currentPath, filename)
+        if os.path.isfile(src):
+            copyfile(src, dst)
+        else:
+            copytree(src, dst)
+
+    if dict(request.form).get('mov'):
+        for filename in fileList:
+            src = os.path.join(session['filePrePath'], filename)
+            if os.path.isfile(src):
+                os.remove(src)
+            else:
+                rmtree(src)
+
+    session.pop('isCopy', False)
+    session.pop('isMov', False)
+
+    flash("%s success!" % fileList)
+
+    return redirect('/ftp/%s' % currentPath)
+
+
+@app.route('/cancel', methods=['POST'])
+def cancel_post():
+    currentPath = session['currentPath']
+
+    file_pop()
+    session.pop('isCopy', False)
+    session.pop('isMov', False)
+
+    return redirect('/ftp/%s' % currentPath)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
